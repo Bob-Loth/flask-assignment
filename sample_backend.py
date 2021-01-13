b83b2ebd87fb25ebca850380a183433cd1091398
Bob-Loth <bob94loth@gmail.com>
2021-01-13 10:41:51 -0800

    added a delete operation to /users

--- sample_backend.py
+++ sample_backend.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
 app = Flask(__name__)
 users = { 
    'users_list' :
@@ -34,16 +35,32 @@ users = {
 @app.route('/')
 def hello_world():
 	return 'Hello, world!'
-@app.route('/users')
+@app.route('/users', methods=['GET', 'POST', 'DELETE'])
 def get_users():
-   search_username = request.args.get('name') # accessing the value of parameter 'name'
-   if search_username:
-      subdict = {'users_list' : []}
-      for user in users['users_list']:
-         if user['name'] == search_username:
-            subdict['users_list'].append(user)
-      return subdict
-   return users
+   if request.method == 'GET':
+      search_username = request.args.get('name')
+      if search_username :
+         subdict = {'users_list' : []}
+         for user in users['users_list']:
+            if user['name'] == search_username:
+               subdict['users_list'].append(user)
+         return subdict
+      return users
+   elif request.method == 'POST':
+      userToAdd = request.get_json()
+      users['users_list'].append(userToAdd)
+      resp = jsonify(success=True)
+      #resp.status_code = 200 #optionally, you can always set a response code. 
+      # 200 is the default code for a normal response
+      return resp
+   elif request.method == 'DELETE':
+      userToDelete = request.get_json()
+      try:
+         users['users_list'].remove(userToDelete)
+         resp = jsonify(success=True)
+      except ValueError:
+         resp = jsonify(success=False)
+      return resp
 @app.route('/users/<id>')
 def get_user(id):
    if id :
