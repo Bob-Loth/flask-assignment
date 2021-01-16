5271a7bb77a24a9cee7250234581ec7aec39744b
Bob-Loth <bob94loth@gmail.com>
2021-01-16 15:28:42 -0800

    response is now the added user, with ID attached, instead of T/F

--- sample_backend.py
+++ sample_backend.py
@@ -62,7 +62,7 @@ def get_users():
       userToAdd = addID(userToAdd)
       users['users_list'].append(userToAdd)
       
-      resp = jsonify(success=True)
+      resp = jsonify(userToAdd)
       resp.status_code = 201 #optionally, you can always set a response code. 
       # 200 is the default code for a normal response
       return resp
